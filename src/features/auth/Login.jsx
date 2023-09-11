5a2e4c3b64682e3ae2dc49e86151d68f3dc5bba0
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-11 11:11:05 -0600

    set up tailwind

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,99 +1,100 @@
-import { useRef, useState, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useRef, useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux'
-import { setCredentials } from './authSlice'
-import { useLoginMutation } from './authApiSlice'
+import { useDispatch } from 'react-redux';
+import { setCredentials } from './authSlice';
+import { useLoginMutation } from './authApiSlice';
 
 const Login = () => {
-    const emailRef = useRef()
-    const errRef = useRef()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [errMsg, setErrMsg] = useState('')
+  const emailRef = useRef();
+  const errRef = useRef();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [login, { isLoading }] = useLoginMutation()
+  const [login, { isLoading }] = useLoginMutation();
 
-    useEffect(() => {
-        emailRef.current.focus()
-    }, [])
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [email, password])
+  useEffect(() => {
+    setErrMsg('');
+  }, [email, password]);
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const { accessToken } = await login({ email, password }).unwrap()
-            dispatch(setCredentials({ accessToken }))
-            setEmail('')
-            setPassword('')
-            navigate('/dashboard')
-        } catch (err) {
-            if (!err.status) {
-                setErrMsg('No Server Response');
-            } else if (err.status === 400) {
-                setErrMsg('Missing Email or Password');
-            } else if (err.status === 401) {
-                setErrMsg('Unauthorized');
-            } else {
-                setErrMsg(err.data?.message);
-            }
-            errRef.current.focus();
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { accessToken } = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setEmail('');
+      setPassword('');
+      navigate('/dashboard');
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg('No Server Response');
+      } else if (err.status === 400) {
+        setErrMsg('Missing Email or Password');
+      } else if (err.status === 401) {
+        setErrMsg('Unauthorized');
+      } else {
+        setErrMsg(err.data?.message);
+      }
+      errRef.current.focus();
     }
+  };
 
-    const handleEmailInput = (e) => setEmail(e.target.value)
-    const handlePwdInput = (e) => setPassword(e.target.value)
+  const handleEmailInput = (e) => setEmail(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
 
-    const errClass = errMsg ? "errmsg" : "offscreen"
+  const errClass = errMsg ? 'errmsg' : 'offscreen';
 
-    if (isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
-    const content = (
-        <section className="public">
-            <header>
-                <h3>Sign in</h3>
-            </header>
-            <main className="login">
-                <p ref={errRef} className={errClass} aria-live="assertive">{errMsg}</p>
+  const content = (
+    <section className="public">
+      <header>
+        <h3>Sign in</h3>
+      </header>
+      <main className="login">
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
 
-                <form className="form" onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                        className="form__input"
-                        type="text"
-                        id="email"
-                        ref={emailRef}
-                        value={email}
-                        onChange={handleEmailInput}
-                        autoComplete="off"
-                        required
-                    />
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email Address</label>
+          <input
+            className="form__input"
+            type="text"
+            id="email"
+            ref={emailRef}
+            value={email}
+            onChange={handleEmailInput}
+            autoComplete="off"
+            required
+          />
 
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        className="form__input"
-                        type="password"
-                        id="password"
-                        onChange={handlePwdInput}
-                        value={password}
-                        required
-                    />
-                    <button className="form__submit-button">Sign In</button>
-                </form>
-            </main>
-            <p>Don't have an account {" "}
-                <Link to="/signup">signup here</Link>
-            </p>
-        </section>
-    )
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+          <button className="form__submit-button">Sign In</button>
+        </form>
+      </main>
+      <p>
+        Don't have an account <Link to="/signup">signup here</Link>
+      </p>
+    </section>
+  );
 
-    return content
-}
-export default Login
+  return content;
+};
+export default Login;
