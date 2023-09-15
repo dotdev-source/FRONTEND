30ff6cb2b8964f2a61581157600a7d46aa11e5cb
“Xaxxoo” <bnahmad83@gmail.com>
2023-09-15 12:00:40 +0100

    User Roles Defined

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const SideNav = () => {
+  const { fullname, status } = useAuth();
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
   
-  return <div><h1>SideNav</h1></div>;
+  return <div>
+    <p>{fullname}</p>
+    <p>{status}</p>
+    <h1>SideNav</h1></div>;
 };
 
 export default SideNav;
