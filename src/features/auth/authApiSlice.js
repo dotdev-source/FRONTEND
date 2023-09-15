9909aa5606f6c9ef0249927117ea0f73fc116d27
“Xaxxoo” <bnahmad83@gmail.com>
2023-09-15 10:04:49 +0100

    Use Persist Login

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,26 +1,28 @@
 import { apiSlice } from "../../app/api/apiSlice"
-import { logOut } from "./authSlice"
+import { logOut, setCredentials } from "./authSlice"
 
 export const authApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         login: builder.mutation({
             query: credentials => ({
-                url: '/admin/login',
+                url: '/auth',
                 method: 'POST',
                 body: { ...credentials }
             })
         }),
         sendLogout: builder.mutation({
             query: () => ({
-                url: '/admin/logout',
+                url: '/auth/logout',
                 method: 'POST',
             }),
             async onQueryStarted(arg, { dispatch, queryFulfilled }) {
                 try {
-                    const { data } =  await queryFulfilled
-                    //console.log(data)
+                    const { data } = await queryFulfilled
+                    console.log(data)
                     dispatch(logOut())
-                    dispatch(apiSlice.util.resetApiState())
+                    setTimeout(() => {
+                        dispatch(apiSlice.util.resetApiState())
+                    }, 1000)
                 } catch (err) {
                     console.log(err)
                 }
@@ -28,9 +30,19 @@ export const authApiSlice = apiSlice.injectEndpoints({
         }),
         refresh: builder.mutation({
             query: () => ({
-                url: '/admin/refresh',
+                url: '/auth/refresh',
                 method: 'GET',
-            })
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled
+                    console.log(data)
+                    const { accessToken } = data
+                    dispatch(setCredentials({ accessToken }))
+                } catch (err) {
+                    console.log(err)
+                }
+            }
         }),
     })
 })
