de83ea468a406f7b1c9a00db6b9cdfc8cac1ce3d
“Xaxxoo” <bnahmad83@gmail.com>
2023-09-18 07:57:30 +0100

    prefetch refactored

--- src/features/auth/Prefetch.jsx
+++ src/features/auth/Prefetch.jsx
@@ -1,43 +1,21 @@
-import { store } from '../../app/store'
-import { teachersApiSlice } from '../teachers/teachersApiSlice'
-import { studentsApiSlice } from '../students/studentsApiSlice';
-import { schoolApiSlice } from '../school/schoolApiSlice';
-import { subjectsApiSlice } from '../subjects/subjectsApiSlice';
-import { programsApiSlice } from '../programs/programsApiSlice';
-import { classesApiSlice } from '../classes/classesApiSlice';
-import { examsApiSlice } from '../exams/examsApiSlice';
-import { academicYearApiSlice } from '../academicYear/academicYearApiSlice';
-import { academicTermApiSlice } from '../academicTerm/academicTermApiSlice';
-import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { store } from "../../app/store";
+import { schoolsApiSlice } from "../teachers/teachersApiSlice";
+
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
 const Prefetch = () => {
-    useEffect(() => {
-        console.log('subscribing')
-        const teachers = store.dispatch(teachersApiSlice.endpoints.getTeachers.initiate())
-        const students = store.dispatch(studentsApiSlice.endpoints.getStudents.initiate())
-        const schools = store.dispatch(schoolApiSlice.endpoints.getSchools.initiate())
-        const subjects = store.dispatch(subjectsApiSlice.endpoints.getSubjects.initiate())
-        const programs = store.dispatch(programsApiSlice.endpoints.getPrograms.initiate())
-        const classes = store.dispatch(classesApiSlice.endpoints.getClasses.initiate())
-        const exams = store.dispatch(examsApiSlice.endpoints.getExams.initiate())
-        const academicYears = store.dispatch(academicYearApiSlice.endpoints.getAcademicYears.initiate())
-        const academicTerms= store.dispatch(academicTermApiSlice.endpoints.getAcademicTerms.initiate())
+  useEffect(() => {
+    // console.log('subscribing')
+    store.dispatch(
+      schoolsApiSlice.util.prefetch("getSchools", "schoolsList", {
+        force: true,
+      })
+    );
 
-        return () => {
-            console.log('unsubscribing')
-            teachers.unsubscribe()
-            students.unsubscribe()
-            schools.unsubscribe()
-            subjects.unsubscribe()
-            programs.unsubscribe()
-            classes.unsubscribe()
-            exams.unsubscribe()
-            academicYears.unsubscribe()
-            academicTerms.unsubscribe()
-        }
-    }, [])
+   
+  }, []);
 
-    return <Outlet />
-}
-export default Prefetch
+  return <Outlet />;
+};
+export default Prefetch;
